cf3cd0ed51cafa0bb3a2ecd01b9a5e2c159d9911
B4rtosek <B4rtosek@gmail.com>
2018-10-26 15:05:46 +0200

    Dashboard and Pageboard edit

--- hosting/static/js/my_js.js
+++ hosting/static/js/my_js.js
@@ -10,3 +10,17 @@ $(document).ready(function () {
         }, 1000, 'swing');
     });
 });
+
+/* Keep the dropdown open !! DELETE DATA-TOQQLE="DROPDOWN"
+$('li.dropdown.keep-inside-clicks-open a').on('click', function (event) {
+    $(this).parent().toggleClass('open');
+});
+
+$('body').on('click', function (e) {
+    if (!$('li.dropdown.keep-inside-clicks-open').is(e.target)
+        && $('li.dropdown.keep-inside-clicks-open').has(e.target).length === 0
+        && $('.open').has(e.target).length === 0
+    ) {
+        $('li.dropdown.keep-inside-clicks-open').removeClass('open');
+    }
+});*/
